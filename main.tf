ea0317a209e6fd2dee5efb4f70d4da2cdf25954b
armenmelkonyan90 <93577909+armenmelkonyan90@users.noreply.github.com>
2022-09-25 14:07:01 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "my-instance" {
   ami                    = var.ami  # ami-09042b2f6d07d164a
   instance_type          = "t2.micro"
   tags = {
-    Name = "my_custom_server"
+    Name = "my_custom_server1"
   }
 }

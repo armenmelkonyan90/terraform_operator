8324c89ea470a5c0b23fe4c3b9e502e61ce8090c
armenmelkonyan90 <93577909+armenmelkonyan90@users.noreply.github.com>
2022-09-25 11:31:26 +0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "my-nginx" {
+  ami                    = var.ami  # ami-09042b2f6d07d164a
+  instance_type          = "t2.micro"
+  tags = {
+    Name = "my_nginx_server"
+  }
+}

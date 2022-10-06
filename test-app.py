a244ad30d58c49bfca8200ec223973a539eab2ca
armenmelkonyan90 <93577909+armenmelkonyan90@users.noreply.github.com>
2022-10-06 11:57:28 +0400

    Update test-app.py

--- test-app.py
+++ test-app.py
@@ -4,4 +4,4 @@ def test_hello():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
-    assert response.data == b'Hello, World!'
+    assert response.data == b'Hellko, World!'
